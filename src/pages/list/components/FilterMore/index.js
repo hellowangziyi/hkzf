15cdc016857c688bb124c9e100c1bc33765f3f24
Everest <1164624362@qq.com>
2022-12-26 22:12:58 +0800

    filterMore样式

--- src/pages/list/components/FilterMore/index.js
+++ src/pages/list/components/FilterMore/index.js
@@ -1,13 +1,34 @@
 import React, { Component } from "react";
-
+import FilterFooter from "../../../../components/FilterFooter";
+import './index.scss'
 export default class FilterMore extends Component {
-  render() {
+  render () {
     const { roomType, oriented, floor, characteristic } = this.props;
     return (
       <div className="root">
-        <div className="mask"></div>
-        <div className="content"></div>
+        {/* 遮罩层 */}
+        <div className="mask" onClick={onCancel} />
+
+        {/* 条件内容 */}
+        <div className="tags">
+          <dl className="dl">
+            <dt className="dt">户型</dt>
+            <dd className="dd">{this.renderFilters(roomType)}</dd>
+
+            <dt className="dt">朝向</dt>
+            <dd className="dd">{this.renderFilters(oriented)}</dd>
+
+            <dt className="dt">楼层</dt>
+            <dd className="dd">{this.renderFilters(floor)}</dd>
+
+            <dt className="dt">房屋亮点</dt>
+            <dd className="dd">{this.renderFilters(characteristic)}</dd>
+          </dl>
+        </div>
+
+        {/* 底部按钮 */}
+        <FilterFooter onCancel={onCancel} onOk={() => { onOk(this.state.selected) }} className="footer" />
       </div>
-    );
+    )
   }
 }
