214bbb1fe5a9f64a7c1964ad062e8a0faa220b14
Everest <49609695+hellowangziyi@users.noreply.github.com>
2022-12-26 18:16:57 +0800

    筛选栏高亮问题完善

--- src/pages/list/components/Filter/index.js
+++ src/pages/list/components/Filter/index.js
@@ -18,26 +18,48 @@ export default class Filter extends Component {
     openType: "",
     // 筛选项数据
     filterData: {},
-    selectedValue:{
-      area:['area','null','null'],
-      mode:['null'],
-      price:['null'],
-      more:[]
-    }
+    selectedValue: {
+      area: ["area", "null"],
+      mode: ["null"],
+      price: ["null"],
+      more: [],
+    },
   };
   componentDidMount() {
     this.getFilterData();
   }
   onTitleClick = (type) => {
+    const { titleSelectedStatus, selectedValue } = this.state;
+    // 创建新状态数组
+    const newtitleSelectedStatus = { ...titleSelectedStatus };
+    Object.keys(newtitleSelectedStatus).forEach((key) => {
+      if (key === type) {
+        // 点击项默认高亮
+        newtitleSelectedStatus[key] = true;
+      } else {
+        //检查是否有选中项
+        const selectedVal = selectedValue[key];
+        if (
+          key === "area" &&
+          (selectedVal.length > 2 || selectedVal[0] !== "area")
+        ) {
+          newtitleSelectedStatus[key] = true;
+        } else if (key === "mode" && selectedVal[0] !== "null") {
+          newtitleSelectedStatus[key] = true;
+        } else if (key === "price" && selectedVal[0] !== "null") {
+          newtitleSelectedStatus[key] = true;
+        } else if (key === "more") {
+          //TODOS:filterMore
+        } else {
+          newtitleSelectedStatus[key] = false;
+        }
+      }
+    });
+    console.log("new", newtitleSelectedStatus);
     //修改state中高亮的值
-    this.setState((prevState) => {
-      return {
-        titleSelectedStatus: {
-          ...prevState.titleSelectedStatus,
-          [type]: true,
-        },
-        openType: type,
-      };
+    this.setState({
+      titleSelectedStatus: newtitleSelectedStatus,
+      openType: type,
     });
   };
   onCancel = () => {
@@ -45,17 +67,16 @@ export default class Filter extends Component {
       openType: "",
     });
   };
-  onSave = (type,value) => {
-    console.log('type',type);
-    console.log('value',value);
+  onSave = (type, value) => {
+    console.log("type", type);
+    console.log("value", value);
     // 隐藏对话框，保存选项值
     this.setState({
       openType: "",
-      selectedValue:{
+      selectedValue: {
         ...this.state.selectedValue,
-        [type]:value
-      }
-     
+        [type]: value,
+      },
     });
   };
   // 获取筛选项的数据
@@ -69,13 +90,13 @@ export default class Filter extends Component {
   }
   // 渲染FilterPicker组件
   renderFilterPicker() {
-    const { openType, filterData,selectedValue } = this.state;
+    const { openType, filterData, selectedValue } = this.state;
     if (openType !== "area" && openType !== "mode" && openType !== "price")
       return null;
     let data = [];
     let cols = 1;
     console.log("filterData[openType]", filterData[openType]);
-    const defaultValue = selectedValue[openType]
+    const defaultValue = selectedValue[openType];
     switch (openType) {
       case "area":
         data = [filterData["area"], filterData["subway"]];
@@ -92,7 +113,7 @@ export default class Filter extends Component {
     }
     return (
       <FilterPicker
-        key={openType}
+        key={openType} //切换tab时可销毁和新建组件
         onCancel={this.onCancel}
         onSave={this.onSave}
         data={data}
@@ -100,6 +121,22 @@ export default class Filter extends Component {
         cols={cols}
         defaultValue={defaultValue}
       ></FilterPicker>
+    );
+  }
+  //渲染FilterMore组件
+  renderFilterMore() {
+    const {
+      openType,
+      filterData: { roomType, oriented, floor, characteristic },
+    } = this.state;
+    if (openType !== "more") return null;
+    return (
+      <FilterMore
+        roomType={roomType}
+        oriented={oriented}
+        floor={floor}
+        characteristic={characteristic}
+      ></FilterMore>
     );
   }
   render() {
--- src/pages/list/components/FilterMore/index.js
+++ src/pages/list/components/FilterMore/index.js
@@ -1,15 +1,13 @@
-import { div } from 'prelude-ls'
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class FilterMore extends Component {
-    render () {
-        return (
-            <div className='root'>
-                <div className='mask'></div>
-                <div className='content'>
-
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const { roomType, oriented, floor, characteristic } = this.props;
+    return (
+      <div className="root">
+        <div className="mask"></div>
+        <div className="content"></div>
+      </div>
+    );
+  }
 }
